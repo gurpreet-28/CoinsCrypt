33f7ca92e7bfbac1f822a6b8a357f23b588123d8
Gurpreet Singh <cynghjimmy0227@gmail.com>
2023-03-31 18:25:05 +0530

    removed newsdata endpoint

--- src/config/api.js
+++ src/config/api.js
@@ -13,7 +13,3 @@ export const SingleCoin = (uuid) => {
 export const HistoricalChart = (uuid, period) => {
   return `https://api.coinranking.com/v2/coin/${uuid}/history/?timePeriod=${period}`;
 };
-
-export const GetNews = (apiKey, page) => {
-  return `https://newsdata.io/api/1/news?apikey=${apiKey}&q=cryptocurrency`;
-};
